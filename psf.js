77babf0759013bcc45714276a324c382c1b62851
Aravind G <g.aravind4@gmail.com>
2018-04-13 10:48:46 +0530

    Delete psf.js

--- psf.js
+++
@@ -1,5 +0,0 @@
-let fs = require('fs'),
-PDFParser = require("pdf2json");
-let pdfParser = new PDFParser();
-x = pdfParser.loadPDF("/home/nineleaps/Downloads/Bidstream targeting.pdf");
-console.log(pdfParser)
